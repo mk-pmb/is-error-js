d02d48c12b8217054e2635e86696ea4086899069
mk-pmb <mk-pmb@users.noreply.github.com>
2016-10-21 23:23:27 +0200

    v2.2.1: [fix] accept Node.js 6.1.0 AssertionErrors
    fixes #4

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var objectToString = Object.prototype.toString;
 var getPrototypeOf = Object.getPrototypeOf;
 var ERROR_TYPE = '[object Error]';
 
-module.exports = isError;
-
-function isError(err) {
+module.exports = function isError(err) {
     if (typeof err !== 'object') {
         return false;
+    }
+    if (err instanceof Error) {
+        // Accept `AssertionError`s from the `assert` module that ships
+        // with Node.js v6.1.0, compare issue #4.
+        return true;
     }
     while (err) {
         if (objectToString.call(err) === ERROR_TYPE) {
@@ -17,4 +20,4 @@ function isError(err) {
         err = getPrototypeOf(err);
     }
     return false;
-}
+};
